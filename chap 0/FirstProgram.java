c1fff82e38cbb33aa115091f4aabd8994c5296f7
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-01-04 00:10:51 +0530

    Chapter 0 committed

---
+++ chap 0/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram{         // class names convention is PascalConvention
+    public static void main(String[]args){      // function names use camelCaseConventions
+        System.out.println("Hello World");
+    }
+}
+
+// Fuctions are like a plumber which does a unique job
+// Class are the collection of these differnt kinds of the functions and also it can be visualised as house hold repairs
+// Hence the class contain functions
