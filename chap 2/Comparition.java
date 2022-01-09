17d18f4b92beb9ba7edb2391b068f45b2970e105
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-01-10 00:26:40 +0530

    Chapter 2 committed

---
+++ chap 2/Comparition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Comparition {
+    public static void main(String[] Comparition){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int a = sc.nextInt();
+
+        if(a>10)
+        System.out.println("Greater");
+
+        else
+        System.out.println("Smaller");
+    }
+}
---
+++ chap 2/One.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class One {
+    public static void main(String [] Benne){
+        Scanner sc = new Scanner(System.in);
+        // double a = 5.5+9;        
+        // int a = 5.5 + 9;     // This gives error as the automatic type casting is not done
+        // System.out.println(a);
+
+        // int a = 8;
+        // System.out.println(++a*2);  // BOth will result in same value 18
+        // System.out.println(a++*2); // Hence both prefix and postfix operator will have same precedence
+
+        // Question 1
+        // float a = 7.0f/4 *9.0f/2;
+        // float a = 7/4 * 9/2;   //Both of these will give different answers as everything is converted to int
+        // System.out.println(a);
+
+        // Question 2 grade encryption
+        // int a = sc.nextInt() + 8;
+        // System.out.println("After Encryption " + a);
+        // System.out.println("After Decryption "+ (a-8));
+
+        // Question 5
+        // int a = 7*49/7 + 35/7;
+        // System.out.println(a);
+    }
+}
